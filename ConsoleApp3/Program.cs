7e84dd158999abacf0713870367bbf38514de307
asara <asara@TENA>
2022-09-21 19:42:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите что-то");
+
+        var str = Console.ReadLine();
+
+        Console.WriteLine("Укажите глубину эха");
+
+        var deep = int.Parse(Console.ReadLine());
+        Echo(str, deep);
+        Console.ReadKey();
+    }
+
+    static void Echo(string saidword, int deep)
+    {
+        Console.WriteLine(saidword);
+        if (deep > 1)
+            Echo(saidword[2..], deep - 1);
+    }
+}
